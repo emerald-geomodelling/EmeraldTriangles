88a3b9383b4365828df7a17ec90ce2f0cafb85e7
Egil <egil.moller@freecode.no>
2020-10-08 10:54:50 +0200

    Better color management

--- emeraldtriangles/plotting_bokeh.py
+++ emeraldtriangles/plotting_bokeh.py
@@ -1,48 +1,70 @@
 import bokeh
 
-def points(fig, cmap, **tri):
-    vertices = tri["vertices"].copy()
-    vertices["color"] = vertices["color"].fillna(-1)
-    source = bokeh.models.ColumnDataSource(data=vertices[["X","Y","color"]])
-    color = {"field": "color", "transform": cmap["transform"]}
+def colormap_default(color, column="color", fixed="red", palette="Inferno256", datasets=[]):
+    datacolumn = False
+    dataset = None
+    for d in datasets:
+        if column in d.columns:
+            datacolumn = column
+            dataset = d
+            break
+    for d in datasets:
+        if color in d.columns:
+            datacolumn = color
+            dataset = d
+            break
+    if datacolumn:
+        return bokeh.transform.linear_cmap(
+            datacolumn, getattr(bokeh.palettes, palette),
+            dataset[datacolumn].min(), dataset[datacolumn].max())
+    return color or fixed
+        
+def points(fig, color=None, **tri):
+    color = colormap_default(color, fixed="green", palette="Greens256", datasets=[tri["vertices"]])
+    source = bokeh.models.ColumnDataSource(data=tri["vertices"])
     fig.circle(x="X", y="Y", color=color, source=source)
 
-def triangles(fig, cmap, **tri):
-    vertices = tri["vertices"].copy()
-    vertices["color"] = vertices["color"].fillna(-1)
+def triangles(fig, color, line_color=None, **tri):
+    color = colormap_default(color, column="color", fixed="red", palette="Inferno256", datasets=[tri["vertices"], tri["triangles"]])
+    vertices = tri["vertices"]
+    triangles = tri["triangles"]
 
     xs = [[[vertices["X"].values[
              [row[1].values[0], row[1].values[1], row[1].values[2], row[1].values[0]]]]]
-          for row in tri["triangles"].iterrows()]
+          for row in triangles.iterrows()]
     ys = [[[vertices["Y"].values[
              [row[1].values[0], row[1].values[1], row[1].values[2], row[1].values[0]]]]]
-          for row in tri["triangles"].iterrows()]
-    color = [(vertices["color"].values[row[1].values[0]]
-              + vertices["color"].values[row[1].values[1]]
-              + vertices["color"].values[row[1].values[2]]) / 3
-              for row in tri["triangles"].iterrows()]
-
-    source = bokeh.models.ColumnDataSource(data={"xs": xs, "ys": ys, "color": color})
-
-    color = {"field": "color", "transform": cmap["transform"]}
+          for row in triangles.iterrows()]
+    data={"xs": xs, "ys": ys}
     
+    colorcol = None
+    if isinstance(color, str) and (color in vertices.columns or color in triangles.columns):
+        colorcol = color
+    elif isinstance(color, dict):
+        colorcol = color["field"]
+
+    if colorcol is not None:
+        if colorcol in triangles.columns:
+            data[colorcol] = triangles[colorcol].values
+        else:
+            data[colorcol] = (  vertices[colorcol].values[triangles[0].values]
+                              + vertices[colorcol].values[triangles[1].values]
+                              + vertices[colorcol].values[triangles[2].values]) / 3
+
+    source = bokeh.models.ColumnDataSource(data=data)
     glyph = bokeh.models.MultiPolygons(xs="xs", ys="ys", fill_color=color, line_color="#8073ac", line_width=1)
     fig.add_glyph(source, glyph)
 
-def plot(fig, cmap = None, **tri):
-    if cmap is None:
-        vertices = tri["vertices"].copy()
-        vertices["color"] = vertices["color"].fillna(-1)
-        cmap = bokeh.transform.linear_cmap(
-            "color", bokeh.palettes.Inferno256,
-            vertices["color"].min(), vertices["color"].max())
-
+def plot(fig, color = None, **tri):
+    if "vertices" in tri:
+        tri["vertices"] = tri["vertices"].copy().fillna(-1)
+    
     if "points" in tri and len(tri["points"]):
-        points(fig, cmap, **tri)
+        points(fig, color, **tri)
     if 'segments' in tri:
         pass
     if 'triangles' in tri and len(tri["triangles"]):
-        triangles(fig, cmap, **tri)
+        triangles(fig, color, **tri)
     if 'holes' in tri:
         pass
     if 'edges' in tri:
