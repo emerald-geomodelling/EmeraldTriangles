7d21a8bbd1acf3449fee5235bda4d9ae8150f065
Egil <egil.moller@freecode.no>
2021-01-20 11:21:28 +0100

    Bugfix

--- emeraldtriangles/io/landxml.py
+++ emeraldtriangles/io/landxml.py
@@ -48,8 +48,8 @@ def parse(xmlfile):
         for triangle_node in surface_node.getElementsByTagName('F'):
             idxs = [int(val) for val in triangle_node.childNodes[0].data.split(" ")]
             triangles.append(idxs)
-        triangles = triangles - 1 # LandXML points are numbered from 1, not 0
         surface["triangles"] = pd.DataFrame(triangles, columns=(0, 1, 2), dtype=int)
+        surface["triangles"] = surface["triangles"] - 1 # LandXML points are numbered from 1, not 0
 
         surfaces[surface_node.getAttributeNode("name").value] = surface
 
