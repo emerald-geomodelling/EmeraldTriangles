cd8cc6b69b17baf413b95762b541db7bdf1de0a3
Egil <egil.moller@freecode.no>
2021-02-16 16:44:28 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='emeraldtriangles',
-    version='0.0.2',
+    version='0.0.3',
     description='Triangle mesh transforms',
     long_description='Iteratively add points to an existing mesh, calculate mesh bounding polygons etc.',
     long_description_content_type="text/markdown",
