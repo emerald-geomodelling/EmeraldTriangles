dd94129ab36f614082876166f4b5fe7ee247f89c
Egil <egil.moller@freecode.no>
2021-04-29 17:15:41 +0200

    Pyvista export functionality

--- setup.py
+++ setup.py
@@ -38,7 +38,8 @@ setuptools.setup(
         "matplotlib",
         "shapely",
         "geoalchemy2",
-        "pyproj"
+        "pyproj",
+        "pyvista"
     ],
     setup_requires=[
         'setuptools>=18.0',
