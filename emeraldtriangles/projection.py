dbcb0fc7b8d1f2d6f2e91b1441ca6a00de3d1c12
Egil <egil.moller@freecode.no>
2021-04-30 10:21:19 +0200

    Added some documentation

--- emeraldtriangles/projection.py
+++ emeraldtriangles/projection.py
@@ -2,11 +2,38 @@ import numpy as np
 import pyproj
 
 def standard_reproject(projection=None, replace=False, **tri):
+    """Reproject the coordinates of a triangulation to web mercator
+    and lat/lon and store the results in the columns x_web/y_web and
+    lon/lat respectively.
+    """
     tri = reproject(projection=projection, replace=replace, projection_out=3857, x_col_out="x_web", y_col_out="y_web", **tri)
     tri = reproject(projection=projection, replace=replace, projection_out=4326, x_col_out="lon", y_col_out="lat", **tri)
     return tri
 
-def reproject(projection=None, projection_out=3857, x_col="X", y_col="Y", x_col_out="x_web", y_col_out="y_web", replace=False, **tri):
+def reproject(projection=None, projection_out=None, x_col="X", y_col="Y", x_col_out="X", y_col_out="Y", replace=False, **tri):
+    """Reproject a triangulation or add additional columns with a
+    secondary projection. Projections are specified as epsg codes.
+    
+    Parameters
+    projection : int
+      The current projection (crs) of x_col and y_col. Defaults to tri["meta"]["projection"]
+    projection_out : int
+      The projection (crs) to reproject the coordinates to.
+    x_col : str
+      The x coordinate column to reproject from. Defaults to "X".
+    y_col : str
+      The y coordinate column to reproject from. Defaults to "Y".
+    x_col_out : str
+      The x coordinate column to reproject to. Defaults to "X".
+    y_col_out : str
+      The x coordinate column to reproject to. Defaults to "Y".
+    replace : bool
+      Replace existing values in the output columns. If false,
+      reprojection is only done on rows with NaN in the output
+      columns.
+    **tri
+      Triangulation to reproject
+    """
     if "meta" not in tri:
         tri["meta"] = {}
         
@@ -15,6 +42,9 @@ def reproject(projection=None, projection_out=3857, x_col="X", y_col="Y", x_col_
 
     if x_col_out in ("X", "x"):
         tri["meta"]["projection"] = projection_out
+
+    if x_col == x_col_out:
+        replace = True
         
     vertices = tri["vertices"]
     if x_col_out not in vertices.columns:
