8e2288f3b6bbc39c9c63a98fbf38a97b3d877e20
Egil <egil.moller@freecode.no>
2021-04-30 11:10:09 +0200

    Added some docs

--- emeraldtriangles/distance.py
+++ emeraldtriangles/distance.py
@@ -2,6 +2,10 @@ import scipy.spatial.distance
 import numpy as np
 
 def distance_to_data(col, x_col="X", y_col="Y", **tri):
+    """Calculate spatial distance (cartesian distance in the current
+    projection) to vertices with non-NaN values in the column col and
+    store the distance in the column col_dist.
+    """
     XA = tri["vertices"][[x_col,y_col]].values
     filt = ~tri["vertices"][col].isna()
     XB = tri["vertices"].loc[filt][[x_col,y_col]].values
