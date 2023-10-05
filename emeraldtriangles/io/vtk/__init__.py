61648328d27b863bc4e37efec300a9e971225c4f
Ed Harrison <eh@emeraldgeo.no>
2023-10-05 11:25:43 +0200

    return vol or surface

--- emeraldtriangles/io/vtk/__init__.py
+++ emeraldtriangles/io/vtk/__init__.py
@@ -3,7 +3,7 @@ import numpy as np
 from . import surface
 from . import volume
 
-def dump(tri, filename, layer_depths=None, **kw):
+def dump(tri, filename=None, layer_depths=None, **kw):
     """Dumps a surface based on your triangles. If layer_depths is
        specified the TIN is interpreted as a 2.5d volume, and
        triangular prisms are generated, with heights from the
@@ -12,6 +12,9 @@ def dump(tri, filename, layer_depths=None, **kw):
        x_col="X", y_col="Y", z_col="Z"
     """
     if layer_depths is not None:
-        volume.to_pyvista(tri, layer_depths=layer_depths, **kw).save(filename)
+        poly = volume.to_pyvista(tri, layer_depths=layer_depths, **kw)
     else:
-        surface.to_pyvista(tri, **kw).save(filename)
+        poly = surface.to_pyvista(tri, **kw)
+    if filename is not None:
+        poly.save(filename)
+    return poly
